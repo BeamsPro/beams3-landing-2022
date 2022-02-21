47cf86d22ec3b2f99988ed90fed64abd0a49a318
Yohanson555 <yohanson555@gmail.com>
2022-02-21 23:49:01 +0300

    слайд влево-право и отзывы готовы

--- js/script.js
+++ js/script.js
@@ -6,14 +6,41 @@ var repeatCookieDelay = 15000;
 var fadeDuration = 500;
 var switchSpeed = 500;
 
+var feedback = [
+	{
+		company: "Pfizer1",
+		text: "Спасибо вам огромное! Вы волшебники! Не знаю как работает ваша магия, но это чудесно! Вы крутые)"
+	},
+	{
+		company: "Pfizer2",
+		text: "Спасибо вам огромное! Вы волшебники! Не знаю как работает ваша магия, но это чудесно! Вы крутые)"
+	},
+	{
+		company: "Pfizer3",
+		text: "Спасибо вам огромное! Вы волшебники! Не знаю как работает ваша магия, но это чудесно! Вы крутые)"
+	},
+	{
+		company: "Pfizer4",
+		text: "Спасибо вам огромное! Вы волшебники! Не знаю как работает ваша магия, но это чудесно! Вы крутые)"
+	},
+	{
+		company: "Pfizer5",
+		text: "Спасибо вам огромное! Вы волшебники! Не знаю как работает ваша магия, но это чудесно! Вы крутые)"
+	},
+	{
+		company: "Pfizer6",
+		text: "Спасибо вам огромное! Вы волшебники! Не знаю как работает ваша магия, но это чудесно! Вы крутые)"
+	},
+];
+
 // functions
 
 
-function cookiesNo(){
-	if(cookieTimer) {
+function cookiesNo() {
+	if (cookieTimer) {
 		clearTimeout(cookieTimer);
 	}
-	
+
 	$("#cookies_block").fadeTo(fadeDuration, 0, () => {
 		cookieTimer = setTimeout(() => {
 			$("#cookies_block").fadeTo(fadeDuration, 1);
@@ -22,8 +49,8 @@ function cookiesNo(){
 }
 
 function cookiesYes() {
-	$.cookie("cookie_applied", 'true', { expires : 10 }); 
-	
+	$.cookie("cookie_applied", 'true', { expires: 10 });
+
 	$("#cookies_block").fadeTo(fadeDuration, 0, () => {
 		$("#cookies_block").remove();
 	});
@@ -45,32 +72,63 @@ function cookiesInit() {
 	}
 }
 
-function goglobal_open(){
+function goglobal_open() {
 
-    document.getElementById('goglobal_block').style.display = 'block';
- 
+	document.getElementById('goglobal_block').style.display = 'block';
+
 }
 
-function goglobal_close(){
+function goglobal_close() {
 
-    document.getElementById('goglobal_block').style.display = 'none';
+	document.getElementById('goglobal_block').style.display = 'none';
 }
 
-function admenu_open(){
-	
-	if(ad == 0){
+function admenu_open() {
+
+	if (ad == 0) {
 		document.getElementById('admenu').style.display = 'block';
 		ad = 1;
-	}else{
+	} else {
 		document.getElementById('admenu').style.display = 'none';
 		ad = 0;
 	}
-	
+
+}
+
+function initLineMove(lineId, ops) {
+	let gap = 0;
+	let absSpeed = Math.abs(ops.speed);
+
+	const move = () => {
+		var ml = parseInt($("#" + lineId).css("margin-left"), 10);
+		ml += ops.speed;
+
+		gap += absSpeed;
+
+		if (gap >= ops.gap) {
+			
+			if (ops.speed > 0) {
+				let child = $("#" + lineId).children().last()
+				$("#" + lineId).prepend(child);
+				ml -= gap;
+			} else {
+				let child = $("#" + lineId).children().first()
+				$("#" + lineId).append(child);
+				ml += gap;
+			}
+
+			gap = 0;
+		}
+
+		$("#" + lineId).css("margin-left", ml + 'px');
+	};
+
+	setInterval(move, 16);
 }
 
 function initFAQ() {
-	$("[rel='faq']").each((i,e) => {
-		$(e).find(".que_block_open_but").on("click", function() {
+	$("[rel='faq']").each((i, e) => {
+		$(e).find(".que_block_open_but").on("click", function () {
 			$(this).toggleClass("opened");
 			$(e).find(".que_text").slideToggle();
 		});
@@ -79,33 +137,33 @@ function initFAQ() {
 
 function initSwitch() {
 	const scopesActivity = {};
-	
+
 	$("[rel='switch']").each((i, e) => {
 		let dataId = $(e).attr("data-id");
 		let scope = $(e).attr("data-scope");
-		
-		if($(e).hasClass("go_active")) {
+
+		if ($(e).hasClass("go_active")) {
 			scopesActivity[scope] = dataId;
 		}
-		
+
 		$(e).on("click", (evt) => {
 			console.log(e);
 			evt.preventDefault();
-			
+
 			let current = scopesActivity[scope];
-			
+
 			$("[data-scope='" + scope + "']").removeClass("go_active");
 			$(e).addClass("go_active");
-			
-			$("#" + current).stop().fadeTo(switchSpeed, 0, function() {
+
+			$("#" + current).stop().fadeTo(switchSpeed, 0, function () {
 				$(this).hide();
 				scopesActivity[scope] = dataId;
 				$("#" + dataId).fadeTo(switchSpeed, 1);
 			});
-			
+
 			return false;
 		});
-	});	
+	});
 }
 
 function initFilter() {
@@ -114,19 +172,19 @@ function initFilter() {
 	$("[rel='filter']").each((i, e) => {
 		$(e).on("click", (evt) => {
 			evt.preventDefault();
-			
+
 			let scope = $(e).attr('data-scope');
 			let filter = $(e).attr('data-filter');
-			
-			$('a[data-scope="'+scope+'"]').removeClass('go_active');
+
+			$('a[data-scope="' + scope + '"]').removeClass('go_active');
 			$(e).addClass('go_active');
-			
+
 			//console.log("scope:", scope);
 			//console.log("filter:", filter);
-			
+
 			if (scope) {
 				let items = filterScopes[scope];
-				
+
 				if (!filter) {
 					$(items).show();
 				} else {
@@ -139,26 +197,81 @@ function initFilter() {
 					}
 				}
 			}
-			
+
 		});
 	});
-	
+
 	$("[rel='filtered']").each((i, e) => {
 		let scope = $(e).attr('data-scope');
 		let filter = $(e).attr('data-filter');
-		
+
 		if (filter) {
 			e.filter = filter.split(',');
 		}
-		
+
 		if (scope) {
 			if (!filterScopes[scope]) {
 				filterScopes[scope] = [];
 			}
-			
+
 			filterScopes[scope].push(e);
 		}
 	});
+}
+
+function initFeedback() {
+	let current = 0;
+	let pads = [];
+
+	const next = () => {
+		if (current >= feedback.length - 1) {
+			current = 0;
+		} else {
+			current += 1;
+		}
+
+		select(current);
+	}
+
+	const prev = () => {
+		if (current <= 0) {
+			current = feedback.length - 1;
+		} else {
+			current -= 1;
+		}
+
+		select(current);
+	}
+
+	const select = (index) => {
+		$(".review_case_icopad").removeClass('go_active');
+
+		if (feedback[index]) {
+			$("#feedback").stop().fadeTo(500, 0, function () {
+				$("#feedback_text").text(feedback[index].text);
+				$("#feedback_company").text(feedback[index].company);
+
+				$(this).fadeTo(500, 1);
+			});
+
+			
+			$(pads[index]).addClass('go_active');
+		}
+	}
+
+	for (let i = 0; i < feedback.length; i++) {
+		let pad = $('<div>').addClass('review_case_icopad');
+		pads.push(pad);
+
+		$(pad).on('click', () => select(i));
+
+		$("#feedback_pads").prepend(pad);
+	}
+
+	$("#prev_feedback").on("click", prev);
+	$("#next_feedback").on("click", next);
+
+	select(0);
 }
 
 // init
@@ -168,4 +281,16 @@ $(() => {
 	initFAQ();
 	initSwitch();
 	initFilter();
+
+	initFeedback();
+
+	initLineMove("line_to_left", {
+		speed: 2,
+		gap: 380,
+	});
+
+	initLineMove("line_to_right", {
+		speed: -2,
+		gap: 380,
+	});
 });
